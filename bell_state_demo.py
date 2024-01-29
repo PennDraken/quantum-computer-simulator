c8ac891cdd008d1bd06f020912fb64ee8417061d
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2024-01-29 17:06:50 +0100

    implemented bell_state (no measurement yet)

--- bell_state_demo.py
+++ bell_state_demo.py
@@ -1,2 +1,17 @@
-import numpy
+import numpy as np
+import random
 
+# gates initialization
+cnot_gate = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]], dtype=complex)
+hadamard_gate = (1/np.sqrt(2))*np.array([[1,1],[1,-1]], dtype=complex)
+
+# qubits
+qubit1_state = np.array([1,0], dtype=complex)
+qubit2_state = np.array([1,0], dtype=complex)
+
+# creating the bell state
+qubit1_state = hadamard_gate.dot(qubit1_state)
+two_qubit_state = np.kron(qubit1_state, qubit2_state) # kron() is the tensor product
+bell_state = cnot_gate.dot(two_qubit_state)
+
+print(bell_state)
