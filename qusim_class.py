5ba2a4e7c98b3eabfa640f9c37f83c88006cc228
henryst@chalmers.se <henry.stromgren@gmail.com>
2024-02-21 17:40:24 +0100

    fixed formatting

--- qusim_class.py
+++ qusim_class.py
@@ -14,16 +14,12 @@ class Qubit():
         self.name = name
 
 # System of a single/ multiple qubits
-
-
 class Register():
     def __init__(self, qubits,  vector: np.array):
         self.qubits = qubits
         self.vector = vector
 
 # System of all registers of qubits
-
-
 class System():
     def __init__(self):
         self.qubits = []  # Used to store order of qubits
@@ -33,47 +29,12 @@ class System():
         # self.qubits.append(qubit)
         self.registers.append(Register([qubit], vector))
 
+    # Apply a gate to a single qubit
     def apply_gate(self, gate, qubit):
         register = find_register(self, qubit)
         register_index = self.registers.index(register)
         register = apply_gate_register(register, qubit, gate)
         self.registers[register_index] = register
-
-    # Does not work for reverse
-    def apply_gate_multiple2(self, gate: np.array, qubit_a, qubit_b):
-        register_a = find_register(self, qubit_a)
-        register_b = find_register(self, qubit_b)
-        register = register_a
-        # merge registers into a single register if theyre not already merged
-        if register_a != register_b:
-            register = combine_registers(register_a, register_b)
-        # swap so that registers are next to each other
-        qubits = register.qubits
-        index_a = qubits.index(qubit_a)
-        index_b = qubits.index(qubit_b)
-        swaps = abs(index_b - index_a) - 1
-        if index_b > index_a + 1:
-            for i in range(0, swaps):
-                register = swap_left(register, qubit_b)
-        elif index_a > index_b + 1:
-            for i in range(0, swaps):
-                register = swap_left(register, qubit_a)
-        # apply expanded gate
-        index = min(index_a, index_b)
-        expanded_gate = expand_gate(gate, index, len(register.qubits))
-        register.vector = register.vector.dot(expanded_gate)
-        # swap back
-        if index_b > index_a:
-            for i in range(0, swaps):
-                register = swap_right(register, qubit_b)
-        elif index_a > index_b:
-            for i in range(0, swaps):
-                register = swap_right(register, qubit_a)
-        # set register_a to register to update state
-        self.registers[self.registers.index(register_a)] = register
-        # delete register_b
-        if register_a != register_b:
-            self.registers.remove(register_b)
 
     # Applies a 2 input gate to 2 qubits
     def apply_gate_multiple(self, gate: np.array, qubit_a, qubit_b):
@@ -102,12 +63,12 @@ class System():
         if register_a != register_b:
             self.registers.remove(register_b)
 
-    # Sets the entirety to one register
+    # Combines all registers to one register and updates state
     def register_combined(self):
         register = self.as_register()
         self.registers = [register]
 
-    # Returns all registers as a single register
+    # Returns all registers as a single register (does not update system state)
     def as_register(self):
         vector = self.registers[0].vector
         qubits = self.registers[0].qubits
@@ -116,7 +77,6 @@ class System():
             vector = np.kron(vector, register.vector)
             qubits += register.qubits
         return Register(vector=vector, qubits=qubits)
-
 
 # ----------------------------------------------------------------------------------------------------
 # METHODS
@@ -173,10 +133,7 @@ def swap(register: Register, qubit_a, qubit_b):
     return register
 
 # Swaps bits located at i and j
-
 # swaps bits (most significant bit has index 0, least significant has index n)
-
-
 def swap_bits(num: int, i: int, j: int, n: int):
     # Extract the bits at positions i and j
     bit_i = (num >> (n - 1 - i)) & 1
@@ -194,8 +151,6 @@ def check_adjacent(register, qubit_a, qubit_b):
     return abs(index_a-index_b) == 1
 
 # Applies gate to a single qubit in a register
-
-
 def apply_gate_register(register, qubit, gate: np.array):
     # Expand gate based on index of qubit
     qubit_index = register.qubits.index(qubit)
@@ -219,18 +174,6 @@ def apply_gate_register(register, qubit, gate: np.array):
 # print(q.registers[0].vector)
 # print(q.registers[1].vector)
 # q.apply_gate_multiple(Gates.CNOT, ["A","B"])
-
-
-def show_swap():
-    state1 = np.kron([0, 1j], [1, 0])
-    state1 = np.kron(state1, [1, 0])
-    print(state1)
-    state = np.kron([1, 0], [1, 0])
-    state = np.kron(state, [0, 1j])
-    print(state)
-    gate = np.kron(Gates.I, Gates.SWAP).dot(np.kron(Gates.SWAP, Gates.I))
-    print(state.dot(gate))
-
 
 # q = System()
 # q.add_qubit("A", np.array([1,0]))
