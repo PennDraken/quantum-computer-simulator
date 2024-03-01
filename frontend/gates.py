10f0d633633e22174240cc5dc435fd2b557821a6
randomUsernameN5 <98349183+randomUsernameN5@users.noreply.github.com>
2024-03-01 22:51:45 +0100

    Update gates.py

--- frontend/gates.py
+++ frontend/gates.py
@@ -1,8 +1,7 @@
 import pygame
-import numpy as np
 from enum import Enum
 import Utilities.Colors as Colors
-import Utilities.screenHandler as screenHandler
+import screenHandler
 
 Loc = screenHandler.Loc
 screen = screenHandler.screen
@@ -19,6 +18,12 @@ class Gate:
         rect = pygame.Rect(xpos, ypos, width, height)
         pygame.draw.rect(screen,Colors.white,rect,0)
         screen.blit(pygame.font.SysFont('Times New Roman', 45).render(gate, True, (170, 200, 200)), (xpos+4, ypos-3))
+        return rect
+    
+
+    
+    def gCollider(self):
+        rect = pygame.Rect(self.x, self.y, self.width, self.height)
         return rect
         
 class gateHandler:
@@ -46,34 +51,27 @@ class gateHandler:
         return self.gateMap[key]
 
 
-    def addGate(self, gate : str, qubits, calculations : [str], relative_position : tuple, collumn): # integrating evenhandler for this method
-        #pygame.draw.rect(screen, colorWhite, (self.xStart, self.yStart, self.gateWidth, self.gateHeight), 0)
-
+    def addGate(self, gate : str, qubits, calculations : [str], relative_position : tuple, column): # integrating evenhandler for this method
+        grid_size = 50
         nrQubits = len(qubits)
         if nrQubits < 1: # should be at least one qubit
             raise ValueError
 
-        #self.addGateCount(qubits[0])
-        #self.step += 1
         pygame.font.init()
-        
-        #dy = relative_position[1] - 75
-
         if nrQubits > 1:
             for i in range(1, nrQubits):
-                x = relative_position[0] + 50 * collumn + 20
-                y = relative_position[1] + (qubits[i] * 50) + 25
-                screenHandler.drawQline((x, (relative_position[1] + (qubits[0] * 50)) + 20), (x, y))
+                x = relative_position[0] + grid_size * column + 20
+                y = relative_position[1] + (qubits[i] * grid_size) + 25
+                screenHandler.drawQline((x, (relative_position[1] + (qubits[0] * grid_size)) + 20), (x, y))
             for i in range(1, nrQubits):
-                x = relative_position[0] + 50 * collumn + 20
-                y = relative_position[1] + (qubits[i] * 50) + 25
+                x = relative_position[0] + grid_size * column + 20
+                y = relative_position[1] + (qubits[i] * grid_size) + 25
                 # need to change this
                 screenHandler.drawQlineMod((x, y), (x, y), Loc.NONE, Loc.END_FILLED)
 
-        x = relative_position[0] + 50 * collumn
-        y = relative_position[1] + (qubits[0] * 50)
-        gate = Gate(gate, x, y, self.gateWidth, self.gateHeight)
-        Gate.renderGate(gate.gate, x, y, self.gateWidth, self.gateHeight)
-        wasClicked = pygame.mouse.get_pressed()[0]
-        # below is an alternative way of handeling calculations
-      
+        x = relative_position[0] + grid_size * column
+        y = relative_position[1] + (qubits[0] * grid_size)
+        gate = Gate(gate, x, y, self.gateWidth, self.gateHeight) # New gate object
+        Gate.renderGate(gate.gate, x, y, self.gateWidth, self.gateHeight) # Render gate 
+        return gate # for gate shifting
+       
