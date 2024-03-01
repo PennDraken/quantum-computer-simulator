bf123e189dddb0dfe2be9aef3bcd7468a9886276
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2024-03-01 19:39:11 +0100

    Changed button width and height

--- frontend/Fields/circuit_navigation_window.py
+++ frontend/Fields/circuit_navigation_window.py
@@ -10,8 +10,9 @@ class Circuit_Navigation_Window:
         self.x = x
         self.y = y
         self.width = screen.get_width()
-        self.height = 100
-        self.button_size = 90
+        self.height = 50
+        self.button_width = 100
+        self.button_height = 50
         self.circuit = circuit
         self.title_font = pygame.font.Font(None, 24)
         self.state_font = pygame.font.Font(None, 20)
@@ -24,26 +25,26 @@ class Circuit_Navigation_Window:
         pygame.draw.rect(self.screen, Colors.black, (self.x, self.y, self.width, self.height))
         pygame.draw.rect(self.screen, Colors.white, (self.x, self.y, self.width, self.height), width=1)
         # draw squares at equal distance
-        box_y = self.height/2-self.button_size/2
+        box_y = self.height/2-self.button_height/2
         for i in range(0, len(self.options)):
             distance_between_boxes = self.width / len(self.options)
-            box_x = distance_between_boxes * i + distance_between_boxes/2 - self.button_size/2
+            box_x = distance_between_boxes * i + distance_between_boxes/2 - self.button_width/2
             # Draw button square
-            pygame.draw.rect(self.screen, Colors.white, (box_x, box_y, self.button_size, self.button_size), width = 2)
+            pygame.draw.rect(self.screen, Colors.white, (box_x, box_y, self.button_width, self.button_height))
             # Draw button text centered on square
-            text(self.screen, self.options[i], box_x + self.button_size/2, box_y + self.button_size/2, Colors.white, self.title_font)
+            text(self.screen, self.options[i], box_x + self.button_width/2, box_y + self.button_height/2, Colors.black, self.title_font)
 
     # Handles mouse input
     def click(self, x, y):
         # update width
         self.width = self.screen.get_width()
         # iterate through all buttons
-        box_y = self.height/2-self.button_size/2
+        box_y = self.height/2-self.button_height/2
         for i in range(0, len(self.options)):
             distance_between_boxes = self.width / len(self.options)
-            box_x = distance_between_boxes * i + distance_between_boxes/2 - self.button_size/2
+            box_x = distance_between_boxes * i + distance_between_boxes/2 - self.button_width/2
             # check click collision
-            if x > box_x and x < box_x + self.button_size and y > box_y and y < box_y + self.button_size:
+            if x > box_x and x < box_x + self.button_width and y > box_y and y < box_y + self.button_height:
                 if self.options[i]=="Run":
                     self.circuit.run()
                 elif self.options[i]=="<<":
--- frontend/Utilities/Colors.py
+++ frontend/Utilities/Colors.py
@@ -1,6 +1,7 @@
 # Constant definitions for different colors
 
 unselected = (127,127,127)
+gray = (127,127,127)
 selected = (250,250,250)
 base = (250,250,250) # White
 hover = (255, 200, 100)
