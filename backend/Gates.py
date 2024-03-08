Fixed the calculation of n in U^2^(n-1)

backend/Gates.py
@@ -44,6 +44,7 @@ def QFT(N : int)->np.array:
             temp = np.power(W, n * m)
             Matrix[n][m] = temp
     Matrix *= constant
+    return Matrix
 
 def DFT(N : int)-> np.array:
     W = np.power(np.e, (-2 * np.pi*1j)/N)
@@ -53,6 +54,7 @@ def DFT(N : int)-> np.array:
         for m in range(1, N):
             Matrix[n][m] = np.power(W, n * m)
     Matrix *= constant
+    return Matrix
 
 # gets corresponding gate from a gate_str
 def string_to_gate(gate_str : str):
@@ -75,23 +77,36 @@ def string_to_gate(gate_str : str):
         case _ :
             return None # TODO Error
 
-def checkIfCoprime(a, b): # Checks if two numbers are coprime, i.e. their greatest common divisor is 1, i.e. gcd(a,b) = 1
+def checkIfCoprime(a, b)->bool: # Checks if two numbers are coprime, i.e. their greatest common divisor is 1, i.e. gcd(a,b) = 1
     while b != 0:
         a, b = b, a % b
-    return a == 1 # If a is 1, then a and b are coprime
+    return a # If a is 1, then a and b are coprime
 
-def findPeriod(a, N): # Finds the period of a function f(x) = a^x mod N
-    if not checkIfCoprime(a, N):
-        return None
+def checkIfNodd(n : int)->bool:
+    return n % 2 != 0
+
+def find_n(N: int)->int:
+    return int(np.ceil(np.log2(N)))
+
+# This is the function we want the quantum subroutine to find. This is a working example in classical code potentially.
+def findPeriod(a, N)->int: # Finds the period of a function f(x) = a^x mod N
     for r in range(a, N):
         if a**r % N == 1:
             return r
     return None
 
 def amodN(a: int, N: int)->np.array:
-    #Controlled multiplication by a mod N
+    if not 1 < a < N or not checkIfNodd(N):
+        return None
+    
+    if checkIfCoprime(a, N) != 1: # If a and N are not coprime, then we a is a factor of N
+        return N/a # Returns the other factor of N
+    
     strings = []
-    for i in range(0, findPeriod(a, N)):
-        x = 2**i
-        strings.append(f"{a}^{x} mod {15}")
+    for i in range(0, 2*find_n(N)): # 2*n-1 is the maximum number of qubits needed
+        x = 2**i      
+        strings.append(f"{a}^{x} mod {15}")   
     return strings
+
+print(amodN(7, 15))
+