037f20314d52f9a54fccf70e89f5028709066731
randomUsernameN5 <98349183+randomUsernameN5@users.noreply.github.com>
2024-03-20 15:13:17 +0100

    Update screenHandler.py

--- frontend/screenHandler.py
+++ frontend/screenHandler.py
@@ -8,12 +8,17 @@ pygame.init()
 screen = pygame.display.set_mode((800, 700), pygame.RESIZABLE)
 pygame.display.set_caption("Kvantis")
 
+
+offsetMod = 0
+
 class Loc(Enum):
     NONE = 0;
     START_FILLED = 1; 
     END_FILLED = 2
     START_CROSS = 3; 
     END_CROSS = 4;
+    START_CROSS2 = 5; 
+    END_CROSS2 = 6;
 
 
 # Draws a line between 2 points on the canvas (used for rendering qubit lines and connections)
@@ -42,8 +47,17 @@ def draw_qubit_connection_point(mod : Loc, start : tuple, end : tuple):
         case Loc.END_FILLED:
             pygame.draw.circle(screen, Colors.white, end, 8) 
         case Loc.START_CROSS: 
-            pygame.draw.circle(screen, Colors.white, start, 15, 2) #change 
+            #pygame.draw.circle(screen, Colors.white, start, 15, 2) #change 
+            pygame.draw.circle(screen,Colors.white,start,UI.gate_size / 3, 3) # move these to screen handler 
+            draw_qubit_line((start[0] - (UI.gate_size / 3),start[1]), (start[0] + (UI.gate_size / 3), start[1]), Colors.white)
+            draw_qubit_line((start[0],start[1] - (UI.gate_size / 3)), (start[0], start[1]  + (UI.gate_size / 3)), Colors.white)
         case Loc.END_CROSS:
             pygame.draw.circle(screen, Colors.white, end, 15, 2) #change
+        case Loc.START_CROSS2:
+            draw_qubit_line((start[0] - (UI.gate_size / 3),start[1] - (UI.gate_size / 3)), (start[0] + (UI.gate_size / 3), start[1] + (UI.gate_size / 3)), Colors.white)
+            draw_qubit_line((start[0] + (UI.gate_size / 3),start[1] - (UI.gate_size / 3)), (start[0] - (UI.gate_size / 3), start[1]  + (UI.gate_size / 3)), Colors.white)
+        case Loc.END_CROSS2:
+           draw_qubit_line((end[0] - (UI.gate_size / 3),end[1] - (UI.gate_size / 3)), (end[0] + (UI.gate_size / 3), end[1] + (UI.gate_size / 3)), Colors.white)
+           draw_qubit_line((end[0] + (UI.gate_size / 3),end[1] - (UI.gate_size / 3)), (end[0] - (UI.gate_size / 3), end[1]  + (UI.gate_size / 3)), Colors.white)
         case Loc.NONE:
             pass
