904ac2a58a625661878d04e7551ad06b6d18aa93
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2024-04-05 14:31:24 +0200

    Grovers algorithm (rough draft)

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -3,7 +3,7 @@
 import numpy as np
 # Returns the instructions for the circuit of the shor subroutine
 def shor_subroutine_circuit(guess : int, N : int):
-    n = (np.ceil(np.log2(N)))
+    n = int((np.ceil(np.log2(N))))
     qubits = []
     for i in range(n*3):
         qubits.append("q" + str(i))
@@ -27,3 +27,13 @@ def shor_subroutine_circuit(guess : int, N : int):
         temp.append("measure " + str(g))
     #print(temp[0:len(temp)])
     return temp
+
+# Grovers algorithm
+# https://i0.wp.com/quantumzeitgeist.com/wp-content/uploads/QZ-Grover-4.png?fit=2427%2C708&ssl=1
+def grover():
+    return [["q0","q1","q2","q3","ancilla 0","ancilla 1","ancilla 2","ancilla 3","ancilla 4"],"H 0","H 1","H 2","H 3",
+            "TOFFOLI 0 1 4","TOFFOLI 2 3 5","CNOT 4 5","TOFFOLI 0 3 6","X 6","TOFFOLI 5 6 7","CNOT 7 8",
+            "TOFFOLI 5 6 7","X 6","TOFFOLI 0 3 6","CNOT 4 5","TOFFOLI 2 3 5","TOFFOLI 0 1 4",
+            "H 0","H 1","H 2","H 3","X 0","X 1","X 2","X 3","I 0","I 1","I 2","H 3",
+            "TOFFOLI 0 1 4","TOFFOLI 2 3 4","TOFFOLI 0 1 4","I 0","I 1","I 2","H 3","X 0","X 1","X 2","X 3","H 0","H 1","H 2","H 3",
+            "measure 0","measure 1","measure 2","measure 3"]
